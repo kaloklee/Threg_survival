c0380bb414f4706ff865b641e757c3e27b423671
Ka Lok Lee <112717156+kaloklee@users.noreply.github.com>
2023-12-28 22:51:53 -0500

    Update threg_stan.R

--- threg_stan.R
+++ threg_stan.R
@@ -33,6 +33,8 @@ data_list <- list(
 )
 
 
+###########################################################################
+
 file <- file.path("threg.stan")
 mod <- cmdstan_model(file, stanc_options = list("O1"), quiet=TRUE)
 
@@ -79,3 +81,50 @@ ggsurvplot(fit.km, risk.table = F)$plot +
   geom_line(data = treatment1_ribbons, aes( x=month, y=mean, color='model1')) 
   
   
+###############################################################
+
+
+file <- file.path("threg_normal.stan")
+mod <- cmdstan_model(file, stanc_options = list("O1"), quiet=TRUE)
+
+
+fit_threg2 <- mod$sample(
+  data = data_list,
+  seed = 123,
+  init = 2,
+  chains = 4,
+  parallel_chains = 4,
+  refresh = 500,
+  #  output_dir = "~/temp"
+  #  iter_warmup = 1000,
+  #  iter_sampling = 1000,
+  #  adapt_delta = .8
+  
+)
+
+fit_threg2$summary(variables = c("a_mu","a_sig","b_mu","b_lny0"))
+
+## fit the threshold regression model on the factor variable "f.treatment2"  
+standard.threg <- threg(Surv(weeks, relapse) ~ f.treatment2 | f.treatment2, data = lkr)
+standard.threg
+## coefficients are similar to the posterior means from Stan
+
+treatment0 <- fit_threg2$summary(c("Survival0"))
+
+treatment0_ribbons <-
+  treatment0 %>%
+  select(mean, q5, q95) %>%
+  mutate(month = c(1:41))
+
+
+treatment1 <- fit_threg2$summary(c("Survival1"))
+
+treatment1_ribbons <-
+  treatment1 %>%
+  select(mean, q5, q95) %>%
+  mutate(month = c(1:41))
+
+
+ggsurvplot(fit.km, risk.table = F)$plot + 
+  geom_line(data = treatment0_ribbons, aes( x=month, y=mean, color='model0')) +
+  geom_line(data = treatment1_ribbons, aes( x=month, y=mean, color='model1')) 
